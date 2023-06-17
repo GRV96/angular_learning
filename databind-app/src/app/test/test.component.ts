3e6d627cb6d58ffcd5970d796dabad3dac401661
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-06-17 19:08:17 -0400

    TestComponent displayed

--- databind-app/src/app/test/test.component.ts
+++ databind-app/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-
+  appName = "My first app in Angular 8";
 }
