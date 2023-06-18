0c78b094403192a62cf81fb3a6f2278372d0fb30
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-06-18 16:41:36 -0400

    ng generate directive custom-style

--- directive-app/src/app/app.module.ts
+++ directive-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
+import { CustomStyleDirective } from './custom-style.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     StructuralDirectivesComponent,
-    AttributeDirectivesComponent
+    AttributeDirectivesComponent,
+    CustomStyleDirective
   ],
   imports: [
     BrowserModule
